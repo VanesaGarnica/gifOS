0e340201edb01b65f5cb9a467bef3242d0318549
Vanesa Garnica <vanesagarnicarodriguez@gmail.com>
2020-02-19 22:42:56 -0300

    first commit

---
+++ js/functions.js
@@ -0,0 +1,53 @@
+const api_url = 'https://api.giphy.com/v1/gifs'
+const my_key = 'nS01RK7xAnsFvtF88FSBh8mYdtygo4CZ'
+const endpoints = {
+    search: `${api_url}/search?api_key=${my_key}`,
+    trending: `${api_url}/trending?api_key=${my_key}`,
+    random: `${api_url}/random?api_key=${my_key}&tag=`,
+    upload: `https://upload.giphy.com/v1/gifs?api_key=${my_key}`
+}
+
+const getData = async endpoint => {
+    try {
+        const res = await fetch(endpoint)
+        const data = await res.json()
+        return data.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const source = (imgId, gifId) => {
+    document.getElementById(imgId).src = 'https://i.giphy.com/media/' + gifId + '/giphy.webp'
+}
+
+const getId = async(endpoint, imgId) => {
+    try {
+        const res = await fetch(endpoint)
+        const data = await res.json()
+        return source(imgId, data.data.id)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const renderGifs = (gifs) => {
+    let $container = document.getElementById("search_container")
+    $container.innerHTML = ''
+
+    for (let gif of gifs) {
+        let div = document.createElement('div')
+        let img = document.createElement('img')
+        let fc = document.createElement('figcaption')
+
+        img.setAttribute("width", "286")
+        img.setAttribute("height", "296")
+        img.setAttribute("style", "margin: 15px 14px")
+        img.src = gif.images.downsized.url
+        img.alt = gif.title
+
+        div.appendChild(img)
+        div.appendChild(fc)
+        $container.appendChild(div)
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", async() => {
+    document.querySelector(".night").addEventListener("click", () => {
+        const body = document.querySelector("body");
+        body.classList.add("dark");
+        body.classList.remove("light");
+    });
+
+    document.querySelector(".day").addEventListener("click", () => {
+        const body = document.querySelector("body");
+        body.classList.add("light");
+        body.classList.remove("dark");
+    });
+
+    const search_input = document.querySelector("#search_input");
+    const search_button = document.querySelector("#search_button");
+    const hardcode_suggest = document.querySelector(".hardcode_suggest");
+    const btn_birds = document.querySelector("#btn_birds");
+    const btn_epic = document.querySelector("#btn_epic");
+    const btn_videogames = document.querySelector("#btn_videogames");
+
+    search_input.addEventListener("keyup", () => {
+        console.log("key_up");
+        hardcode_suggest.style.display = "flex";
+    });
+
+    const gifs = await getData(`${endpoints.search}`);
+    renderGifs(gifs);
+
+    const anotherGifs = await getData(`${endpoints.trending}`);
+    const trendingGifs = anotherGifs.splice(0, 15);
+    renderGifs(trendingGifs);
+
+    search_button.addEventListener("click", async e => {
+        console.log("click");
+        hardcode_suggest.style.display = "none";
+        let value = search_input.value.trim();
+
+        if (!value) return;
+
+        const gifs = await getData(`${endpoints.search}&q=${value}`);
+        renderGifs(gifs);
+
+        scroll = document.getElementById("search_container");
+        scroll.scrollIntoView({ behavior: "smooth" });
+    });
+
+    document.querySelector("#sm1").addEventListener("click", () => {
+        gifRandom("Harry Potter", "img1");
+    });
+    document.querySelector("#sm2").addEventListener("click", () => {
+        gifRandom("Gilmore Girls", "img2");
+    });
+    document.querySelector("#sm3").addEventListener("click", () => {
+        gifRandom("Game of thrones", "img3");
+    });
+    document.querySelector("#sm4").addEventListener("click", () => {
+        gifRandom("Disney", "img4");
+    });
+
+    const gifRandom = (tag, imgId) => {
+        getId(`${endpoints.random}` + tag, imgId);
+    };
+
+    gifRandom("Harry Potter", "img1");
+    gifRandom("Gilmore Girls", "img2");
+    gifRandom("Game of thrones", "img3");
+    gifRandom("Disney", "img4");
+
+    btn_birds.addEventListener("click", async e => {
+        const gifs = await getData(`${endpoints.search}&q=${"birds"}`);
+        renderGifs(gifs);
+
+        scroll = document.getElementById("container_hardcode_suggest");
+        scroll.scrollIntoView({ behavior: "smooth" });
+    });
+
+    btn_epic.addEventListener("click", async e => {
+        const gifs = await getData(`${endpoints.search}&q=${"epic"}`);
+        renderGifs(gifs);
+
+        scroll = document.getElementById("container_hardcode_suggest");
+        scroll.scrollIntoView({ behavior: "smooth" });
+    });
+
+    btn_videogames.addEventListener("click", async e => {
+        const gifs = await getData(`${endpoints.search}&q=${"videogames"}`);
+        renderGifs(gifs);
+
+        scroll = document.getElementById("container_hardcode_suggest");
+        scroll.scrollIntoView({ behavior: "smooth" });
+    });
+});
